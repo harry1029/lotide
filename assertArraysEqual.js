afbd5b5012e2cafffeee92c48ff2dc05c5e96772
Harry Jiang <harry10291993@hotmail.com>
2021-10-28 08:23:43 -0400

    Implement assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(String.fromCodePoint(128516) + ` Assertion Passed: [${array1}] === [${array2}]`)
+  } else {
+    console.log(String.fromCodePoint(128534) + ` Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);

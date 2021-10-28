992fb479ab5986af0c43eeec52bcf672d3df923f
Harry Jiang <harry10291993@hotmail.com>
2021-10-28 18:16:57 -0400

    Implement assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,12 @@
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(String.fromCodePoint(128516) + ` Assertion Passed: [${inspect(actual)}] === [${inspect(expected)}]`);
+  } else {
+    console.log(String.fromCodePoint(128534) + ` Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+  }
+};
+
 const eqObjects = function(object1, object2) {
   if(Object.keys(object1).length === Object.keys(object2).length) {
     for (const object1Key of Object.keys(object1)) {
@@ -18,14 +25,6 @@ const eqObjects = function(object1, object2) {
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(String.fromCodePoint(128516) + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(128534) + ` Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -38,16 +37,18 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+assertObjectsEqual(ab, { a: "1", b: "2" });
+assertObjectsEqual(ab, ba);
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+assertObjectsEqual(ba, abc);
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+assertObjectsEqual(cd, dc);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+assertObjectsEqual(cd, { c: "1", d: ["2", 3, 4] });

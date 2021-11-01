a220dc5503d185d0496a549078b5f082d0e10fb5
Harry Jiang <harry10291993@hotmail.com>
2021-11-01 14:34:25 -0400

    Refactor eqArrays test cases into test folder

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(String.fromCodePoint(128516) + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(128534) + ` Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -18,11 +10,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -2,12 +2,4 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(String.fromCodePoint(128516) + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(128534) + ` Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 module.exports = tail;

2152640b3dd0167952d1c1562230c84c813a0184
Harry Jiang <harry10291993@hotmail.com>
2021-11-01 15:58:35 -0400

    Modify middleTest and tailTest to use mocha and chai library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,30 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
+describe("#tail", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
 
-// TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns [2] for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
 
-const words2 = ["Yo Yo"];
-console.log(tail(words2));
-assertEqual(words2.length, 1);
+  it("returns [2, 3] for [1, 2 ,3]", () => {
+    assert.deepEqual(tail([1,2,3]), [2, 3]);
+  });
 
-const words3 = [];
-console.log(tail(words3));
-assertEqual(words3.length, 0);
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+
+  it("returns [2, 3, 4, 5, 6] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5, 6]), [2, 3, 4, 5, 6]);
+  });
+
+  it("returns original length of array (3) for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words); // no need to capture the return value since we are not checking it
+    assert.equal(words.length, 3); // original array should still have 3 elements!
+  });
+});
